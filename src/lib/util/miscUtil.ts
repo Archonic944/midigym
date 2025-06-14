d9f93666dd0e999be76c0f234a7073d9a7e7443f
Archonic <gabrielc2904@gmail.com>
2025-06-14 15:12:17 -0400

    Fix typo

--- src/lib/util/miscUtil.ts
+++ src/lib/util/miscUtil.ts
@@ -17,16 +17,16 @@ export function pcNum(note: string): number{ //C = 0, C# = 1, D = 2, etc
 
 // Builds a chord based on pitch classes starting from C3 (lowest point on our virtual piano)
 export function assignOctaveNumbers(notes: string[]): string[]{
-    
     let octave = 3;
     let finalNotes = [];
     let lastNum = 0;
     for(let note of notes){
+        note = flatToSharp(note);
         let pc = pcNum(note);
         if(pc == -1) console.warn("Pitch class not found in pc map: " + note);
         else if(pc < lastNum) octave++;
         lastNum = pc;
-        finalNotes.push(note + "" + pc);
+        finalNotes.push(note + "" + octave);
     }
     return finalNotes;
 }
