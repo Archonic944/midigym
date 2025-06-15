13f42a230908ac144a52123b0ac9545f2a1d59df
Archonic <gabrielc2904@gmail.com>
2025-06-15 13:59:51 -0400

    Fix typo

--- src/lib/util/miscUtil.ts
+++ src/lib/util/miscUtil.ts
@@ -11,7 +11,7 @@ export function reverseMap<K extends string | number | symbol, A extends string 
     return newRecord;
 }
 
-const pc = ["C", "D#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+const pc = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 export function pcNum(note: string): number{ //C = 0, C# = 1, D = 2, etc
     return pc.indexOf(note);
 }
